1bb7c5695e0bd5cfeb9a1f65257c3c5ba40892e9
yowainwright <yowainwright@gmail.com>
2021-10-31 01:45:52 -0700

    feat: adds working jest & build

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,14 @@
 module.exports = {
-  setupFilesAfterEnv: ['./jest.setup.js'],
   displayName: 'react-endpoint-renderer',
   testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+  },
   testRegex: '(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   moduleNameMapper: {
     '\\.(css)$': 'identity-obj-proxy',
   },
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['./jest.setup.ts', '@testing-library/jest-dom/extend-expect'],
 }
--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
 
+import { theme } from '../styles'
 import { EndpointRendererContent } from '../index'
 
 describe('EndpointRenderer', () => {
   it('renders a string', () => {
-    const { container } = render(<EndpointRendererContent endpoint='test' />)
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <EndpointRendererContent endpoint='test' />
+      </ThemeProvider>,
+    )
     const actual = container?.querySelector('[data-value]')?.textContent
     expect(actual).toEqual('test')
   })
 
   it('renders an array list items', () => {
-    const { container } = render(<EndpointRendererContent endpoint={['test', 'test2']} />)
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <EndpointRendererContent endpoint={['test', 'test2']} />
+      </ThemeProvider>,
+    )
     const actual = container.querySelectorAll('.endpoint__item')
     expect(actual.length).toEqual(2)
     expect(actual[0].getAttribute('data-value')).toEqual('test')
@@ -19,14 +29,22 @@ describe('EndpointRenderer', () => {
   })
 
   it('renders an object', () => {
-    const { container } = render(<EndpointRendererContent endpoint={{ test: 'foo' }} />)
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <EndpointRendererContent endpoint={{ test: 'foo' }} />
+      </ThemeProvider>,
+    )
     const actual = container.querySelectorAll('.endpoint__item')[0]
     expect(actual).toBeDefined()
     expect(actual.textContent).toEqual('test: foo')
   })
 
   it('renders no data', () => {
-    const { container } = render(<EndpointRendererContent endpoint={null} />)
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <EndpointRendererContent endpoint={null} />
+      </ThemeProvider>,
+    )
     const actual = container.querySelector('.endpoint__text--no-data')
     expect(actual).toBeDefined()
     expect(actual?.textContent).toEqual('no data')
