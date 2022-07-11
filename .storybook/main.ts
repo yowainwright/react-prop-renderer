0319867ef909f23155dd58c043b40d9a14b52a3f
yowainwright <yowainwright@gmail.com>
2022-07-11 13:54:17 -0700

    fix: prefer ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 module.exports = {
   stories: ['../stories/*.stories.mdx', '../stories/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async (config) => {
+  webpackFinal: async (config: any) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: [
