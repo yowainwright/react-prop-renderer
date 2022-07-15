0b3753d1baeff79e8c0c7f58e4a57b6085d4aaa9
yowainwright <yowainwright@gmail.com>
2022-07-15 14:25:13 -0700

    fix: switch to vite builder

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,5 @@
 module.exports = {
   stories: ['../stories/*.stories.mdx', '../stories/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async (config: any) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-      ],
-    })
-    config.resolve.extensions.push('.ts', '.tsx')
-    return config
-  },
+  builder: '@storybook/builder-vite',
 }
